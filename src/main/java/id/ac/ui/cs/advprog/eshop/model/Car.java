1b5f01d74e8142f36040bd0b27a664d5fc655043
Akmal76 <akmalramadhan76@gmail.com>
2024-02-16 00:42:33 +0700

    [FEAT]: create Car model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.eshop.model;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Car {
+    private String carId;
+    private String carName;
+    private String carColor;
+    private int carQuantity;
+}
