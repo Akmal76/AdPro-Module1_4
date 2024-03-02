9db030977bd93c790a67935578a6398d71c5e514
Akmal76 <akmalramadhan76@gmail.com>
2024-03-03 00:47:57 +0700

    [RED] Add tests for PaymentTest

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -0,0 +1,60 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentTest {
+    private Map<String, String> paymentData;
+
+    @BeforeEach
+    void setUp() {
+        this.paymentData = new HashMap<>();
+    }
+
+    @Test
+    void testCreatePaymentDefaultStatus() {
+        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b", "VOUCHER", this.paymentData);
+        assertEquals("13652556-012a-4c07-b546-54eb1396d79b", payment.getId());
+        assertEquals("VOUCHER", payment.getMethod());
+        assertEquals("REJECTED", payment.getStatus());
+        assertSame(this.paymentData, payment.getPaymentData());
+    }
+
+    @Test
+    void testCreatePaymentSuccessStatus() {
+        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b", "VOUCHER", this.paymentData, "SUCCESS");
+        assertEquals("13652556-012a-4c07-b546-54eb1396d79b", payment.getId());
+        assertEquals("VOUCHER", payment.getMethod());
+        assertEquals("SUCCESS", payment.getStatus());
+        assertSame(this.paymentData, payment.getPaymentData());
+    }
+
+    @Test
+    void testCreatePaymentInvalidStatus() {
+        assertThrows (IllegalArgumentException.class, () -> {
+            Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b", "VOUCHER", this.paymentData, "INVALID");
+        });
+    }
+
+    @Test
+    void testSetPaymentDataEmpty() {
+        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b", "VOUCHER", this.paymentData);
+        this.paymentData.clear();
+        assertThrows (IllegalArgumentException.class, () -> {
+            payment.setPaymentData(this.paymentData);
+        });
+    }
+
+    @Test
+    void testSetPaymentDataSuccess() {
+        Payment payment = new Payment("13652556-012a-4c07-b546-54eb1396d79b", "VOUCHER", this.paymentData);
+        this.paymentData.put("voucherCode", "ESHOP1234ABC5678");
+        payment.setPaymentData(this.paymentData);
+        assertSame(this.paymentData, payment.getPaymentData());
+    }
+}
