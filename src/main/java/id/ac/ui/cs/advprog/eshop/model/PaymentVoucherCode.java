28501cb56f06bf94bb9d9f836c00c2e6e284b902
Akmal76 <akmalramadhan76@gmail.com>
2024-03-04 21:18:13 +0700

    [GREEN] Implement PaymentVoucherCode class

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucherCode.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucherCode.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import enums.PaymentStatus;
+
 import java.util.Map;
 
 public class PaymentVoucherCode extends Payment {
@@ -13,5 +15,21 @@ public class PaymentVoucherCode extends Payment {
 
     @Override
     public void setPaymentData(Map<String, String> paymentData) {
+        if (paymentData.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            int numericalChar = 0;
+            for (char c : paymentData.get("voucherCode").toCharArray()) {
+                if (Character.isDigit(c)) {
+                    numericalChar++;
+                }
+            }
+            this.paymentData = paymentData;
+            if (paymentData.get("voucherCode").length() == 16 && paymentData.get("voucherCode").startsWith("ESHOP") && numericalChar == 8) {
+                this.status = PaymentStatus.SUCCESS.getValue();
+            } else {
+                this.status = PaymentStatus.REJECTED.getValue();
+            }
+        }
     }
 }
